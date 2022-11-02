bd7b18c1efc50206db1693de25d66eae6cac987c
Klaus Wakonig <klaus.wakonig@psi.ch>
2022-11-02 11:22:28 +0100

    added beamlineinfo to int

--- ophyd_devices/__init__.py
+++ ophyd_devices/__init__.py
@@ -2,5 +2,5 @@ from .galil.galil_ophyd import GalilMotor
 from .npoint.npoint import NPointAxis
 from .rt_lamni import RtLamniMotor
 from .sim.sim import SynAxisMonitor, SynAxisOPAAS, SynFlyer, SynSignalRO, SynSLSDetector
-from .sls_devices.sls_devices import SLSOperatorMessages
+from .sls_devices.sls_devices import BeamlineInfo, SLSOperatorMessages
 from .smaract.smaract_ophyd import SmaractMotor
