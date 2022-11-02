bd8d081c5b4b5511624f9d2606745db0b0a5b1cc
Klaus Wakonig <klaus.wakonig@psi.ch>
2022-11-02 11:18:55 +0100

    added beamlineinfo

--- ophyd_devices/sls_devices/sls_devices.py
+++ ophyd_devices/sls_devices/sls_devices.py
@@ -21,3 +21,22 @@ class SLSOperatorMessages(Device):
 # for i in range(5):
 #     setattr(SLSOperatorMessages, f"message{i}", Cpt(EpicsSignalRO, f"ACOAU-ACCU:OP-MSG{i}", auto_monitor=True))
 #     setattr(SLSOperatorMessages, f"message_date{i}", Cpt(EpicsSignalRO, f"ACOAU-ACCU:OP-DATE{i}", auto_monitor=True))
+
+
+class BeamlineInfo(Device):
+    eh_t02_avg_temperature = Cpt(EpicsSignalRO, "ILUUL-02AV:TEMP", auto_monitor=True)
+    eh_t02_temperature_t0204_axis_16 = Cpt(
+        EpicsSignalRO, "ILUUL-0200-EB104:TEMP", auto_monitor=True
+    )
+    eh_t02_temperature_t0205_axis_18 = Cpt(
+        EpicsSignalRO, "ILUUL-0200-EB105:TEMP", auto_monitor=True
+    )
+    operation = Cpt(EpicsSignalRO, "ACOAU-ACCU:OP-MODE.VAL", auto_monitor=True)
+    injection_mode = Cpt(EpicsSignalRO, "ALIRF-GUN:INJ-MODE", auto_monitor=True)
+    current_threshold = Cpt(EpicsSignalRO, "ALIRF-GUN:CUR-LOWLIM", auto_monitor=True)
+    current_deadband = Cpt(EpicsSignalRO, "ALIRF-GUN:CUR-DBAND", auto_monitor=True)
+    filling_pattern = Cpt(EpicsSignalRO, "ACORF-FILL:PAT-SELECT", auto_monitor=True)
+    filling_life_time = Cpt(EpicsSignalRO, "ARIDI-PCT:TAU-HOUR", auto_monitor=True)
+    orbit_feedback_mode = Cpt(EpicsSignalRO, "ARIDI-BPM:OFB-MODE", auto_monitor=True)
+    fast_orbit_feedback = Cpt(EpicsSignalRO, "ARIDI-BPM:FOFBSTATUS-G", auto_monitor=True)
+    ring_current = Cpt(EpicsSignalRO, "ARIDI-PCT:CURRENT", auto_monitor=True)
