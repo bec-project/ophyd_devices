c7cabc3eeb41d0ded3c5b05e4de3941437f857d0
e20588 <e20588@x12sa-comp-2.psi.ch>
2022-11-11 18:07:23 +0100

    cleanup

--- ophyd_devices/__init__.py
+++ ophyd_devices/__init__.py
@@ -3,5 +3,5 @@ from .galil.galil_ophyd import GalilMotor
 from .npoint.npoint import NPointAxis
 from .rt_lamni import RtLamniMotor
 from .sim.sim import SynAxisMonitor, SynAxisOPAAS, SynFlyer, SynSignalRO, SynSLSDetector
-from .sls_devices.sls_devices import BeamlineInfo, SLSOperatorMessages
+from .sls_devices.sls_devices import SLSInfo, SLSOperatorMessages
 from .smaract.smaract_ophyd import SmaractMotor
