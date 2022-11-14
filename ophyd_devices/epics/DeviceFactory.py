244326937135acec09890544e227fcb85f29e550
Mohacsi Istvan <istvan.mohacsi@psi.ch>
2022-11-14 14:18:58 +0100

    Added device factory

---
+++ ophyd_devices/epics/DeviceFactory.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 13 17:06:51 2021
+
+@author: mohacsi_i
+"""
+
+import os
+import yaml
+from ophyd.ophydobj import OphydObject
+from ophyd import EpicsSignal, EpicsSignalRO, EpicsMotor
+from ophyd.quadem import QuadEM
+import pathlib
+
+
+path = os.path.dirname(os.path.abspath(__file__))
+
+
+from proxies import *
+
+
+# Load SLS common database
+fp = open(f"{path}/db/test_database.yml", "r")
+lut_db = yaml.load(fp, Loader=yaml.Loader)
+
+# Load SLS common database
+fp = open(f"{path}/db/machine_database.yml", "r")
+lut_db = yaml.load(fp, Loader=yaml.Loader)
+
+# Load beamline specific database
+bl = os.getenv('BEAMLINE_XNAME', "X12SA")
+fp = open(f"{path}/db/{bl.lower()}_database.yml", "r")
+lut_db.update(yaml.load(fp, Loader=yaml.Loader))
+
+
+def createProxy(name: str, connect=True) -> OphydObject:
+    """ Factory routine to create an ophyd device with a pre-defined schema.
+        Does nothing if the device is already an OphydObject!
+    """    
+    if issubclass(type(name), OphydObject):
+        return name
+    
+    entry = lut_db[name]   
+    cls_candidate = globals()[entry["type"]]
+    print(f"Device candidate: {cls_candidate}")
+
+    try:
+        if issubclass(cls_candidate, OphydObject):
+            ret = cls_candidate(**entry["config"])
+            if connect:
+                ret.wait_for_connection(timeout=5)
+            return ret
+        else:
+            raise RuntimeError(f"Unsupported return class: {schema}")
+    except TypeError:
+        # Simulated devices
+        if issubclass(type(cls_candidate), OphydObject):
+            return cls_candidate
+        else:
+            raise RuntimeError(f"Unsupported return class: {schema}")     
+
+
+if __name__ == "__main__":
+    pass
+
+
+
---
+++ ophyd_devices/epics/proxies/__init__.py
@@ -0,0 +1,2 @@
+from .DelayGeneratorDG645 import DelayGeneratorDG645
+from .slits import SlitH, SlitV
