19f5f728cc5c224f0802bc88a6084d073d6f78ac
gac-x12sa <gac-x12sa@x12sa-comp-1.psi.ch>
2022-11-14 16:33:47 +0100

    Added insertion device proxy

--- ophyd_devices/epics/proxies/__init__.py
+++ ophyd_devices/epics/proxies/__init__.py
@@ -2,3 +2,4 @@ from .DelayGeneratorDG645 import DelayGeneratorDG645
 from .slits import SlitH, SlitV
 from .XbpmBase import XbpmBase, XbpmCsaxsOp
 from .SpmBase import SpmBase
+from .InsertionDevice import InsertionDevice
