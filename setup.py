412a0e4480a0a5e7d2921cef529ef8aceda90bb7
Klaus Wakonig <klaus.wakonig@psi.ch>
2023-06-28 13:02:02 +0200

    feat: added dev install to setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 from setuptools import setup
 
+__version__ = "0.0.1"
+
 if __name__ == "__main__":
-    setup(install_requires=["ophyd", "typeguard", "prettytable", "pytest"])
+    setup(
+        install_requires=["ophyd", "typeguard", "prettytable", "bec_lib"],
+        extras_require={"dev": ["pytest", "pytest-random-order"]},
+        version=__version__,
+    )
