66d283baeb30da261d0f27e73bca4c9b90d0cadd
wakonig_k <klaus.wakonig@psi.ch>
2023-09-01 10:00:52 +0200

    fix: added pyepics dependency

--- setup.py
+++ setup.py
@@ -4,7 +4,15 @@ __version__ = "0.4.0"
 
 if __name__ == "__main__":
     setup(
-        install_requires=["ophyd", "typeguard", "prettytable", "bec_lib", "numpy", "pyyaml"],
+        install_requires=[
+            "ophyd",
+            "typeguard",
+            "prettytable",
+            "bec_lib",
+            "numpy",
+            "pyyaml",
+            "pyepics",
+        ],
         extras_require={"dev": ["pytest", "pytest-random-order", "black"]},
         version=__version__,
     )
