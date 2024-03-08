d9f09b0d866f97a859c9b437474928e7a9e8c1b6
wakonig_k <klaus.wakonig@psi.ch>
2024-03-08 13:39:34 +0100

    feat: added computed signal

--- ophyd_devices/__init__.py
+++ ophyd_devices/__init__.py
@@ -7,18 +7,19 @@ from .galil.sgalil_ophyd import SGalilMotor
 from .npoint.npoint import NPointAxis
 from .rt_lamni import RtFlomniMotor, RtLamniMotor
 from .sim.sim import SimCamera
-from .sim.sim import SimMonitor
 from .sim.sim import SimFlyer
 from .sim.sim import SimFlyer as SynFlyer
+from .sim.sim import SimMonitor
 from .sim.sim import SimMonitor as SynAxisMonitor
 from .sim.sim import SimMonitor as SynGaussBEC
 from .sim.sim import SimPositioner
 from .sim.sim import SimPositioner as SynAxisOPAAS
 from .sim.sim import SynDeviceOPAAS
+from .sim.sim_frameworks import DeviceProxy, H5ImageReplayProxy, SlitProxy
 from .sim.sim_signals import ReadOnlySignal
-from .sim.sim_frameworks import DeviceProxy, SlitProxy, H5ImageReplayProxy
 from .sim.sim_signals import ReadOnlySignal as SynSignalRO
 from .sls_devices.sls_devices import SLSInfo, SLSOperatorMessages
 from .smaract.smaract_ophyd import SmaractMotor
 from .utils.bec_device_base import BECDeviceBase
+from .utils.dynamic_pseudo import ComputedSignal
 from .utils.static_device_test import launch
