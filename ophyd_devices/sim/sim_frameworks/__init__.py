7abd21233faad3c0b693eecb754fc00520362a10
David Perl <david.perl@psi.ch>
2025-01-17 15:44:30 +0100

    refactor: split device proxies into separate files in a module

---
+++ ophyd_devices/sim/sim_frameworks/__init__.py
@@ -0,0 +1,5 @@
+from ophyd_devices.sim.sim_frameworks.device_proxy import DeviceProxy
+from ophyd_devices.sim.sim_frameworks.h5_image_replay_proxy import H5ImageReplayProxy
+from ophyd_devices.sim.sim_frameworks.slit_proxy import SlitProxy
+
+__all__ = ["DeviceProxy", "H5ImageReplayProxy", "SlitProxy"]
