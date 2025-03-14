d78f588410533f8b2ff73deea3222136f03afc80
gac-x06da <gac-x06da@x06da-bec-001.psi.ch>
2025-03-14 14:17:25 +0100

    Something updated

--- ophyd_devices/devices/__init__.py
+++ ophyd_devices/devices/__init__.py
@@ -5,3 +5,4 @@ from ophyd.sim import SynAxis, SynPeriodicSignal, SynSignal
 from .epics_motor_ex import EpicsMotorEx
 from .sls_devices import SLSInfo, SLSOperatorMessages
 from .SpmBase import SpmBase
+from .psi_motor import EpicsMotorMR, EpicsMotorEC
