97e0862b4c8a7f1e8cc59cad822a35a8968ce875
appel_c <christian.appel@psi.ch>
2026-03-11 18:57:13 +0100

    w

--- ophyd_devices/devices/virtual_slits.py
+++ ophyd_devices/devices/virtual_slits.py
@@ -168,17 +168,24 @@ class SlitWidthSetpoint(_VirtualSlitSignal):
         """Alias for put to adhere to the set interface of a signal."""
         self.check_value(value)
         status = StatusBase(obj=self)
+        callback_received = set()
 
-        def _status_callback(success, exception=None, **kwargs):
+        def _status_callback(success, **kwargs):
             with self._rlock:
                 if status.done:
                     return
-                if success:
-                    status.set_finished()
-                else:
+                if success is False:
                     if exception is None:
-                        exception = UnknownStatusFailure(f"{self.name} failed to move to {value}")
+                        exception = RuntimeError(f"{self.name} failed to move to {value}")
                     status.set_exception(exception)
+                else:
+                    if (
+                        self._positioner_low.name in callback_received
+                        and self._positioner_high.name in callback_received
+                    ):
+                        status.set_finished()
+                    else:
+                        callback_received.add(kwargs["obj"].name)
 
         pos_low, pos_high = self.get_positions_low_high()
         center = (pos_high + pos_low) / 2
